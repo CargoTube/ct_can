e28eae1c59d5c32e7bd83f04f85a9a2d6c42f4c0
bwegh <baswegh@gmx.de>
2017-02-12 10:30:37 +0100

    change to ct_train

--- src/ct_train.erl
+++ src/ct_train.erl
@@ -1,30 +1,30 @@
 %%
-%% Copyright (c) 2014-2016 Bas Wegh
+%% Copyright (c) 2014-2017 Bas Wegh
 %%
 
 %% @private
--module(sibo_proto).
+-module(ct_train).
 -author("Bas Wegh").
--include("sibo_proto_mapping.hrl").
+-include("ct_train_mapping.hrl").
 
 -export([
-         deserialize/2,
-         serialize/2,
+         load_cargo/2,
+         unload_cargo/2,
          ping/1,
          pong/1
         ]).
 
 -define(JSONB_SEPARATOR, <<24>>).
 
-deserialize(Buffer, Encoding) ->
+load_cargo(Buffer, Encoding) ->
     BinaryEncodings = [raw_msgpack, raw_json, msgpack_batched, raw_erlbin],
     IsBinaryEnc = lists:member(Encoding, BinaryEncodings),
-    deserialize_bin_or_text(IsBinaryEnc, Buffer, Encoding).
+    load_bin_or_text(IsBinaryEnc, Buffer, Encoding).
 
 
-serialize(WampMap, Enc) ->
-  WampMsg = sibo_proto_converter:to_wamp(WampMap),
-  serialize_message(WampMsg, Enc).
+unload_cargo(WampMap, Enc) ->
+  WampMsg = ct_train_loading:unload(WampMap),
+  unload_message(WampMsg, Enc).
 
 ping(Payload) ->
     add_binary_frame(1, Payload).
@@ -32,46 +32,46 @@ ping(Payload) ->
 pong(Payload) ->
     add_binary_frame(2, Payload).
 
-deserialize_bin_or_text(true, Buffer, Encoding) ->
-    deserialize_binary(Buffer, [], Encoding);
-deserialize_bin_or_text(false, Buffer, Encoding) ->
-    deserialize_text(Buffer, [], Encoding).
+load_bin_or_text(true, Buffer, Encoding) ->
+    load_binary(Buffer, [], Encoding);
+load_bin_or_text(false, Buffer, Encoding) ->
+    load_text(Buffer, [], Encoding).
 
 
 %% @private
--spec deserialize_text(Buffer :: binary(), Messages :: list(),
+-spec load_text(Buffer :: binary(), Messages :: list(),
                        Encoding :: atom()) ->
     {[Message :: map()], NewBuffer :: binary()}.
-deserialize_text(Buffer, Messages, msgpack) ->
+load_text(Buffer, Messages, msgpack) ->
   case msgpack:unpack_stream(Buffer, []) of
     {error, incomplete} ->
       {to_erl_reverse(Messages), Buffer};
     {error, Reason} ->
       error(Reason);
     {Msg, NewBuffer} ->
-      deserialize_text(NewBuffer, [Msg | Messages], msgpack)
+      load_text(NewBuffer, [Msg | Messages], msgpack)
   end;
-deserialize_text(Buffer, Messages, json) ->
+load_text(Buffer, Messages, json) ->
     case jsone:try_decode(Buffer, []) of
         {ok, Msg, NewBuffer} ->
-            {[sibo_proto_converter:to_erl(Msg) | Messages], NewBuffer};
+            {[ct_train_loading:load(Msg) | Messages], NewBuffer};
         _ ->
             {Messages, Buffer}
     end;
-deserialize_text(Buffer, _Messages, json_batched) ->
+load_text(Buffer, _Messages, json_batched) ->
   Wamps = binary:split(Buffer, [?JSONB_SEPARATOR], [global, trim]),
   Dec = fun(M, List) ->
                 [jsone:decode(M, []) | List]
         end,
   {to_erl_reverse(lists:foldl(Dec, [], Wamps)), <<"">>};
-deserialize_text(Buffer, Messages, _) ->
+load_text(Buffer, Messages, _) ->
   {to_erl_reverse(Messages), Buffer}.
 
 %% @private
--spec deserialize_binary(Buffer :: binary(), Messages :: list(),
+-spec load_binary(Buffer :: binary(), Messages :: list(),
                          Encoding :: atom()) ->
   {[Message :: term()], NewBuffer :: binary()}.
-deserialize_binary(<<LenType:32/unsigned-integer-big, Data/binary>> = Buffer,
+load_binary(<<LenType:32/unsigned-integer-big, Data/binary>> = Buffer,
                    Messages, Enc) ->
     <<Type:8, Len:24>> = <<LenType:32>>,
     case {Type, byte_size(Data) >= Len} of
@@ -85,38 +85,38 @@ deserialize_binary(<<LenType:32/unsigned-integer-big, Data/binary>> = Buffer,
                             _ ->
                                 msgpack:unpack(EncMsg, [])
                         end,
-            deserialize_binary(NewBuffer, [Msg | Messages], Enc);
+            load_binary(NewBuffer, [Msg | Messages], Enc);
         {1, true} ->      %Ping
             <<Payload:Len/binary, NewBuffer/binary>> = Data,
-            deserialize_binary(NewBuffer, [#{type => ping, payload => Payload}
+            load_binary(NewBuffer, [#{type => ping, payload => Payload}
                                            | Messages], Enc);
         {2, true} ->      %Pong
             <<Payload:Len/binary, NewBuffer/binary>> = Data,
-            deserialize_binary(NewBuffer, [#{type => pong, payload => Payload}
+            load_binary(NewBuffer, [#{type => pong, payload => Payload}
                                            | Messages], Enc);
         {_, false} ->
             {to_erl_reverse(Messages), Buffer}
     end;
-deserialize_binary(Buffer, Messages, _Enc) ->
+load_binary(Buffer, Messages, _Enc) ->
     {to_erl_reverse(Messages), Buffer}.
 
 %% @private
-serialize_message(Msg, msgpack) ->
+unload_message(Msg, msgpack) ->
   msgpack:pack(Msg, []);
-serialize_message(Msg, msgpack_batched) ->
-  serialize_message(Msg, raw_msgpack);
-serialize_message(Msg, json) ->
+unload_message(Msg, msgpack_batched) ->
+  unload_message(Msg, raw_msgpack);
+unload_message(Msg, json) ->
   jsone:encode(Msg);
-serialize_message(Msg, json_batched) ->
+unload_message(Msg, json_batched) ->
   Enc = jsone:encode(Msg),
   <<Enc/binary, ?JSONB_SEPARATOR/binary>>;
-serialize_message(Message, raw_erlbin) ->
+unload_message(Message, raw_erlbin) ->
   Enc = term_to_binary(Message),
   add_binary_frame(Enc);
-serialize_message(Message, raw_msgpack) ->
+unload_message(Message, raw_msgpack) ->
   Enc = msgpack:pack(Message, []),
   add_binary_frame(Enc);
-serialize_message(Message, raw_json) ->
+unload_message(Message, raw_json) ->
   Enc = jsone:encode(Message),
   add_binary_frame(Enc).
 
@@ -135,4 +135,4 @@ to_erl_reverse(List) ->
 %% @private
 to_erl_reverse([], List) -> List;
 to_erl_reverse([H | T], Messages) ->
-  to_erl_reverse(T, [sibo_proto_converter:to_erl(H) | Messages]).
+  to_erl_reverse(T, [ct_train_loading:load(H) | Messages]).
---
+++ src/ct_train_loading.erl
@@ -0,0 +1,413 @@
+%%
+%% Copyright (c) 2014-2017 Bas Wegh
+%%
+-module(ct_train_loading).
+-author("Bas Wegh, bwegh@github.com").
+
+-include("ct_train_types.hrl").
+-include("ct_train_mapping.hrl").
+-include("ct_train_codes.hrl").
+
+
+%% API
+-export([unload/1, load/1]).
+
+
+%% TODO:  heartbeat
+
+
+-spec unload(ErlWamp) -> list() when
+      ErlWamp :: ct_train().
+unload(ErlWamp) ->
+    true = ct_train_cargo_check:is_safe_cargo(ErlWamp),
+    msg_unload(ErlWamp).
+
+-spec load(WampMsg) -> ct_train() when
+      WampMsg :: list().
+load(WampMsg) ->
+    ErlMsg = msg_load(WampMsg),
+    true = ct_train_cargo_check:is_safe_cargo(ErlMsg),
+    ErlMsg.
+
+
+-spec msg_unload(Msg) -> list() when
+      Msg :: ct_train().
+msg_unload(#{type := hello, realm := Realm, details := Details}) ->
+    [?HELLO, Realm, Details];
+msg_unload(#{type := welcome, session_id := SessionId, details := Details}) ->
+    [?WELCOME, SessionId, Details];
+msg_unload(#{type := abort, details := Details, reason := Reason}) ->
+    [?ABORT, Details, error_unload(Reason)];
+msg_unload(#{type := goodbye, details := Details, reason := Reason}) ->
+    [?GOODBYE, Details, error_unload(Reason)];
+msg_unload(#{type := error, request_type := AtomType, request_id := RequestId,
+          details := Details, error := Error,
+          arguments_kw := ArgumentsKw} = Msg)
+  when is_map(ArgumentsKw), map_size(ArgumentsKw) > 0 ->
+    WampType = atom_to_request_type(AtomType),
+    Arguments = maps:get(arguments, Msg, []),
+    [?ERROR, WampType, RequestId, Details, error_unload(Error), Arguments,
+     ArgumentsKw];
+msg_unload(#{type := error, request_type := AtomType, request_id := RequestId,
+          details := Details, error := Error, arguments := Arguments})
+  when is_list(Arguments), length(Arguments) > 0 ->
+    WampType = atom_to_request_type(AtomType),
+    [?ERROR, WampType, RequestId, Details, error_unload(Error), Arguments];
+msg_unload(#{type := error, request_type := AtomType, request_id := RequestId,
+          details := Details, error := Error}) ->
+    WampType = atom_to_request_type(AtomType),
+    [?ERROR, WampType, RequestId, Details, error_unload(Error)];
+msg_unload(#{type := publish, request_id := RequestId, options := Options,
+              topic := Topic, arguments_kw := ArgumentsKw} = Msg)
+  when is_map(ArgumentsKw), map_size(ArgumentsKw) > 0 ->
+    Arguments = maps:get(arguments, Msg, []),
+    [?PUBLISH, RequestId, Options, Topic, Arguments,
+     ArgumentsKw];
+msg_unload(#{type := publish, request_id := RequestId, options := Options,
+              topic := Topic, arguments := Arguments})
+  when is_list(Arguments), length(Arguments) > 0 ->
+    [?PUBLISH, RequestId, Options, Topic, Arguments];
+msg_unload(#{type := publish, request_id := RequestId, options := Options,
+              topic := Topic}) ->
+    [?PUBLISH, RequestId, Options, Topic];
+msg_unload(#{type := published, request_id := RequestId,
+              publication_id := PublicationId}) ->
+    [?PUBLISHED, RequestId, PublicationId];
+msg_unload(#{type := subscribe, request_id := RequestId, options := Options,
+          topic := Topic}) ->
+    [?SUBSCRIBE, RequestId, Options, Topic];
+msg_unload(#{type := subscribed, request_id := RequestId,
+          subscription_id := SubscriptionId}) ->
+    [?SUBSCRIBED, RequestId, SubscriptionId];
+msg_unload(#{type := unsubscribe, request_id := RequestId,
+          subscription_id := SubscriptionId}) ->
+    [?UNSUBSCRIBE, RequestId, SubscriptionId];
+msg_unload(#{type := unsubscribed, request_id := RequestId}) ->
+    [?UNSUBSCRIBED, RequestId];
+msg_unload(#{type := event, subscription_id := SubscriptionId,
+          publication_id := PublicationId, details := Details,
+          arguments_kw := ArgumentsKw} = Msg)
+  when is_map(ArgumentsKw), map_size(ArgumentsKw) > 0 ->
+    Arguments = maps:get(arguments, Msg, []),
+    [?EVENT, SubscriptionId, PublicationId, Details, Arguments,
+     ArgumentsKw];
+msg_unload(#{type := event, subscription_id := SubscriptionId,
+          publication_id := PublicationId, details := Details,
+          arguments := Arguments})
+  when is_list(Arguments), length(Arguments) > 0 ->
+    [?EVENT, SubscriptionId, PublicationId, Details, Arguments];
+msg_unload(#{type := event, subscription_id := SubscriptionId,
+          publication_id := PublicationId, details := Details}) ->
+    [?EVENT, SubscriptionId, PublicationId, Details];
+msg_unload(#{type := call, request_id := RequestId, options := Options,
+          procedure := Procedure, arguments_kw := ArgumentsKw} = Msg)
+  when is_map(ArgumentsKw), map_size(ArgumentsKw) > 0 ->
+    Arguments = maps:get(arguments, Msg, []),
+    [?CALL, RequestId, Options, Procedure, Arguments,
+     ArgumentsKw];
+msg_unload(#{type := call, request_id := RequestId, options := Options,
+          procedure := Procedure, arguments := Arguments})
+  when is_list(Arguments), length(Arguments) > 0 ->
+    [?CALL, RequestId, Options, Procedure, Arguments];
+msg_unload(#{type := call, request_id := RequestId, options := Options,
+          procedure := Procedure}) ->
+    [?CALL, RequestId, Options, Procedure];
+msg_unload(#{type := result, request_id := RequestId, details := Details,
+         arguments_kw := ArgumentsKw} = Msg)
+  when is_map(ArgumentsKw), map_size(ArgumentsKw) > 0 ->
+    Arguments = maps:get(arguments, Msg, []),
+    [?RESULT, RequestId, Details, Arguments, ArgumentsKw];
+msg_unload(#{type := result, request_id := RequestId, details := Details,
+         arguments := Arguments})
+  when is_list(Arguments), length(Arguments) > 0 ->
+    [?RESULT, RequestId, Details, Arguments];
+msg_unload(#{type := result, request_id := RequestId, details := Details}) ->
+    [?RESULT, RequestId, Details];
+msg_unload(#{type := register, request_id := RequestId, options := Options,
+          procedure := Procedure}) ->
+    [?REGISTER, RequestId, Options, Procedure];
+msg_unload(#{type := registered, request_id := RequestId,
+          registration_id := RegistrationId}) ->
+    [?REGISTERED, RequestId, RegistrationId];
+msg_unload(#{type := unregister, request_id := RequestId,
+          registration_id := RegistrationId}) ->
+    [?UNREGISTER, RequestId, RegistrationId];
+msg_unload(#{type := unregistered, request_id := RequestId}) ->
+    [?UNREGISTERED, RequestId];
+msg_unload(#{type := invocation, request_id := RequestId,
+         registration_id := RegistrationId, details := Details,
+         arguments_kw := ArgumentsKw} = Msg)
+  when is_map(ArgumentsKw), map_size(ArgumentsKw) > 0 ->
+    Arguments = maps:get(arguments, Msg, []),
+    [?INVOCATION, RequestId, RegistrationId, Details, Arguments,
+    ArgumentsKw];
+msg_unload(#{type := invocation, request_id := RequestId,
+         registration_id := RegistrationId, details := Details,
+         arguments := Arguments})
+  when is_list(Arguments), length(Arguments) > 0 ->
+    [?INVOCATION, RequestId, RegistrationId, Details, Arguments];
+msg_unload(#{type := invocation, request_id := RequestId,
+         registration_id := RegistrationId, details := Details}) ->
+    [?INVOCATION, RequestId, RegistrationId, Details];
+msg_unload(#{type := yield, request_id := RequestId, options := Options,
+         arguments_kw := ArgumentsKw} = Msg)
+  when is_map(ArgumentsKw), map_size(ArgumentsKw) > 0 ->
+    Arguments = maps:get(arguments, Msg, []),
+    [?YIELD, RequestId, Options, Arguments, ArgumentsKw];
+msg_unload(#{type := yield, request_id := RequestId, options := Options,
+         arguments := Arguments})
+  when is_list(Arguments), length(Arguments) > 0 ->
+    [?YIELD, RequestId, Options, Arguments];
+msg_unload(#{type := yield, request_id := RequestId, options := Options}) ->
+    [?YIELD, RequestId, Options];
+%% ADVANCED MESSAGES
+msg_unload(#{type := challenge, auth_method := AuthMethod, extra := Extra}) ->
+    [?CHALLENGE, authmethod_unload(AuthMethod), Extra];
+msg_unload(#{type := authenticate, signature := Signature, extra := Extra}) ->
+    [?AUTHENTICATE, Signature, Extra];
+msg_unload(#{type := cancel, request_id := RequestId, options := Options}) ->
+    [?CANCEL, RequestId, Options];
+msg_unload(#{type := interrupt, request_id := RequestId,
+              options := Options}) ->
+    [?INTERRUPT, RequestId, Options].
+%% msg_unload({type := heartbeat, IncomingSeq, OutgoingSeq}) ->
+%%     [?HEARTBEAT, IncomingSeq, OutgoingSeq];
+
+
+
+-spec msg_load(Msg) -> ct_train() when
+      Msg :: list().
+msg_load([?HELLO, Realm, Details]) ->
+    #{type => hello, realm => Realm, details => dict_load(Details)};
+msg_load([?WELCOME, SessionId, Details]) ->
+    #{type => welcome, session_id => SessionId,
+      details => dict_load(Details)};
+msg_load([?ABORT, Details, Reason]) ->
+    #{type => abort, details => dict_load(Details),
+      reason => try_error_load(Reason)};
+msg_load([?GOODBYE, Details, Reason]) ->
+    #{type => goodbye, details => dict_load(Details),
+      reason => try_error_load(Reason)};
+msg_load([?ERROR, RequestType, RequestId, Details, Error, Arguments,
+            ArgumentsKw]) ->
+    ErlType = request_type_to_atom(RequestType),
+    #{type => error, request_type => ErlType, request_id => RequestId,
+      details => Details, error => try_error_load(Error),
+     arguments => Arguments, arguments_kw => ArgumentsKw};
+msg_load([?ERROR, RequestType, RequestId, Details, Error, Arguments]) ->
+    ErlType = request_type_to_atom(RequestType),
+    #{type => error, request_type => ErlType, request_id => RequestId,
+      details => Details, error => try_error_load(Error),
+     arguments => Arguments};
+msg_load([?ERROR, RequestType, RequestId, Details, Error]) ->
+    ErlType = request_type_to_atom(RequestType),
+    #{type => error, request_type => ErlType, request_id => RequestId,
+      details => Details, error => try_error_load(Error)};
+msg_load([?PUBLISH, RequestId, Options, Topic]) ->
+    #{type => publish,  request_id => RequestId,
+      options => dict_load(Options), topic => Topic};
+msg_load([?PUBLISH, RequestId, Options, Topic, Arguments]) ->
+    #{type => publish,  request_id => RequestId,
+      options => dict_load(Options), topic => Topic, arguments => Arguments};
+msg_load([?PUBLISH, RequestId, Options, Topic, Arguments, ArgumentsKw]) ->
+    #{type => publish,  request_id => RequestId,
+      options => dict_load(Options), topic => Topic, arguments => Arguments,
+      arguments_kw => ArgumentsKw};
+msg_load([?PUBLISHED, RequestId, PublicationId]) ->
+    #{type => published, request_id => RequestId,
+      publication_id => PublicationId};
+msg_load([?SUBSCRIBE, RequestId, Options, Topic]) ->
+    #{type => subscribe, request_id => RequestId,
+      options => dict_load(Options), topic => Topic};
+msg_load([?SUBSCRIBED, RequestId, SubscriptionId]) ->
+    #{type => subscribed, request_id => RequestId,
+      subscription_id => SubscriptionId};
+msg_load([?UNSUBSCRIBE, RequestId, SubscriptionId]) ->
+    #{type => unsubscribe, request_id => RequestId,
+      subscription_id => SubscriptionId};
+msg_load([?UNSUBSCRIBED, RequestId]) ->
+    #{type => unsubscribed, request_id => RequestId};
+msg_load([?EVENT, SubscriptionId, PublicationId, Details]) ->
+    #{type => event, subscription_id => SubscriptionId,
+      publication_id => PublicationId, details => dict_load(Details)};
+msg_load([?EVENT, SubscriptionId, PublicationId, Details, Arguments]) ->
+    #{type => event, subscription_id => SubscriptionId,
+      publication_id => PublicationId, details => dict_load(Details),
+      arguments => Arguments};
+msg_load([?EVENT, SubscriptionId, PublicationId, Details, Arguments,
+        ArgumentsKw]) ->
+    #{type => event, subscription_id => SubscriptionId,
+      publication_id => PublicationId, details => dict_load(Details),
+      arguments => Arguments, arguments_kw => ArgumentsKw};
+msg_load([?CALL, RequestId, Options, Procedure]) ->
+    #{type => call, request_id => RequestId, options => dict_load(Options),
+      procedure => Procedure};
+msg_load([?CALL, RequestId, Options, Procedure, Arguments]) ->
+    #{type => call, request_id => RequestId, options => dict_load(Options),
+      procedure => Procedure, arguments => Arguments};
+msg_load([?CALL, RequestId, Options, Procedure, Arguments, ArgumentsKw]) ->
+    #{type => call, request_id => RequestId, options => dict_load(Options),
+      procedure => Procedure, arguments => Arguments,
+      arguments_kw => ArgumentsKw};
+msg_load([?RESULT, RequestId, Details]) ->
+    #{type => result, request_id => RequestId, details => dict_load(Details)};
+msg_load([?RESULT, RequestId, Details, Arguments]) ->
+    #{type => result, request_id => RequestId, details => dict_load(Details),
+      arguments => Arguments};
+msg_load([?RESULT, RequestId, Details, Arguments, ArgumentsKw]) ->
+    #{type => result, request_id => RequestId, details => dict_load(Details),
+      arguments => Arguments, arguments_kw => ArgumentsKw};
+msg_load([?REGISTER, RequestId, Options, Procedure]) ->
+    #{type => register, request_id => RequestId,
+      options => dict_load(Options), procedure => Procedure};
+msg_load([?REGISTERED, RequestId, RegistrationId]) ->
+    #{type => registered, request_id => RequestId,
+      registration_id => RegistrationId};
+msg_load([?UNREGISTER, RequestId, RegistrationId]) ->
+    #{type => unregister, request_id => RequestId,
+      registration_id => RegistrationId};
+msg_load([?UNREGISTERED, RequestId]) ->
+    #{type => unregistered, request_id => RequestId};
+msg_load([?INVOCATION, RequestId, RegistrationId, Details]) ->
+    #{type => invocation, request_id => RequestId,
+      registration_id => RegistrationId, details => dict_load(Details)};
+msg_load([?INVOCATION, RequestId, RegistrationId, Details, Arguments]) ->
+    #{type => invocation, request_id => RequestId,
+      registration_id => RegistrationId, details => dict_load(Details),
+      arguments => Arguments};
+msg_load([?INVOCATION, RequestId, RegistrationId, Details, Arguments,
+            ArgumentsKw]) ->
+    #{type => invocation, request_id => RequestId,
+      registration_id => RegistrationId, details => dict_load(Details),
+      arguments => Arguments, arguments_kw => ArgumentsKw};
+msg_load([?YIELD, RequestId, Options]) ->
+    #{type => yield, request_id => RequestId, options => dict_load(Options)};
+msg_load([?YIELD, RequestId, Options, Arguments]) ->
+    #{type => yield, request_id => RequestId, options => dict_load(Options),
+      arguments => Arguments};
+msg_load([?YIELD, RequestId, Options, Arguments, ArgumentsKw]) ->
+    #{type => yield, request_id => RequestId, options => dict_load(Options),
+      arguments => Arguments, arguments_kw => ArgumentsKw};
+%% ADVANCED MESSAGES
+msg_load([?CHALLENGE, AuthMethod, Extra]) ->
+    #{type => challenge, auth_method => authmethod_load(AuthMethod),
+      extra => dict_load(Extra)};
+msg_load([?AUTHENTICATE, Signature, Extra]) ->
+    #{type => authenticate, signature => Signature,
+      extra => dict_load(Extra)};
+msg_load([?CANCEL, RequestId, Options]) ->
+    #{type => cancel, request_id => RequestId, options => dict_load(Options)};
+msg_load([?INTERRUPT, RequestId, Options]) ->
+    #{type => interrupt, request_id => RequestId,
+      options => dict_load(Options)};
+msg_load(#{type := ping} = Ping) ->
+    Ping;
+msg_load(#{type := pong} = Pong) ->
+    Pong.
+%% msg_load([?HEARTBEAT, IncomingSeq, OutgoingSeq, _Discard]) ->
+%%     msg_load([?HEARTBEAT, IncomingSeq, OutgoingSeq]);
+%% msg_load([?HEARTBEAT, IncomingSeq, OutgoingSeq]) ->
+%%     #{type => heartbeat, sequence_in => IncomingSeq,
+%%       sequence_out => OutgoingSeq};
+
+
+-spec try_error_load(Error) -> binary() | atom() when
+      Error :: binary().
+try_error_load(Error) ->
+    convert_value(load, Error, ?ERROR_MAPPING).
+
+
+-spec error_unload(Error) -> binary() when
+      Error :: binary() | atom().
+error_unload(Error) ->
+    convert_value(unload, Error, ?ERROR_MAPPING).
+
+
+-spec dict_load(Dict)  -> map() when
+      Dict :: map().
+dict_load(Dict) ->
+    value_load(Dict).
+
+
+-spec value_load(Value) -> map() | list() | number() | binary() when
+      Value :: map() | list() | number() | binary() | atom().
+value_load(Map) when is_map (Map) ->
+    PropList = maps:to_list(Map),
+    Convert = fun({Key, Value}, NewMap) ->
+                NewKey = try_to_atom(Key),
+                NewValue = value_load(Value),
+                maps:put(NewKey, NewValue, NewMap)
+              end,
+    lists:foldl(Convert, #{}, PropList);
+value_load(Binary) when is_binary(Binary) ->
+    try_to_atom(Binary);
+value_load(List) when is_list(List) ->
+    Convert = fun(Element, NewList) ->
+                      NewElement = value_load(Element),
+                      [NewElement | NewList]
+              end,
+    lists:reverse(lists:foldl(Convert, [], List));
+value_load(N) when is_number(N) ->
+    N.
+
+
+-spec try_to_atom(Binary) -> binary() | atom() when
+      Binary :: binary().
+try_to_atom(Binary) ->
+    try binary_to_existing_atom(Binary, utf8) of
+        Atom -> Atom
+    catch _:_ ->
+             Binary
+    end.
+
+
+-spec authmethod_load(Method) -> binary() | atom() when
+      Method :: binary().
+authmethod_load(Method) ->
+    convert_value(load, Method, ?AUTH_METHOD_MAPPING).
+
+-spec authmethod_unload(Method) -> binary() when
+      Method :: atom().
+authmethod_unload(Method) ->
+    convert_value(unload, Method, ?AUTH_METHOD_MAPPING).
+
+-spec request_type_to_atom(RequestType) -> atom() when
+      RequestType :: pos_integer().
+request_type_to_atom(RequestType) ->
+    {RequestType, Atom} = lists:keyfind(RequestType, 1, ?REQUEST_TYPE_MAPPING),
+    Atom.
+
+-spec atom_to_request_type(Atom) -> pos_integer() when
+      Atom :: atom().
+atom_to_request_type(Atom) ->
+    {RequestType, Atom} = lists:keyfind(Atom, 2, ?REQUEST_TYPE_MAPPING),
+    RequestType.
+
+
+
+-spec convert_value(Direction, Value, Mapping) ->  any() when
+      Direction :: load | unload,
+      Value :: any(),
+      Mapping :: list().
+convert_value(Direction, Value, Mapping) ->
+    ValPos = value_pos(Direction),
+    ValueTuple = case lists:keyfind(Value, ValPos, Mapping) of
+                     {EV, WV} -> {EV, WV};
+                     false -> {Value, Value}
+                 end,
+    convert(Direction, ValueTuple).
+
+-spec convert(Direction, Tuple) -> any() when
+      Direction :: load | unload,
+      Tuple :: {atom(), any()}.
+convert(load, {ErlVal, _}) ->
+    ErlVal;
+convert(unload, {_, WampVal}) ->
+    WampVal.
+
+-spec value_pos(Direction) -> 1 | 2 when
+      Direction :: load | unload.
+value_pos(load) ->
+    2;
+value_pos(unload) ->
+    1.
