9296a8e2ceabccf584a5fff4b6d45de089178d6c
bwegh <baswegh@gmx.de>
2017-09-17 17:12:34 +0200

    add newer error uris

--- include/ct_msg_mapping.hrl
+++ include/ct_msg_mapping.hrl
@@ -3,19 +3,23 @@
 %%
 
 -define(ERROR_MAPPING,
-        [ {goodbye_and_out, <<"wamp.error.goodbye_and_out">>},
-          {authorization_failed, <<"wamp.error.authorization_failed">>},
+        [ {authorization_failed, <<"wamp.error.authorization_failed">>},
           {canceled, <<"wamp.error.canceled">>},
           {close_realm, <<"wamp.error.close_realm">>},
           {disclose_not_allowed, <<"wamp.error.disclose_me.not_allowed">>},
+          {goodbye_and_out, <<"wamp.error.goodbye_and_out">>},
           {invalid_argument, <<"wamp.error.invalid_argument">>},
           {invalid_uri, <<"wamp.error.invalid_uri">>},
+          {network_failure, <<"wamp.error.network_failure">>},
+          {no_eligible_callee, <<"wamp.error.no_eligible_callee">>},
           {no_such_procedure, <<"wamp.error.no_such_procedure">>},
           {no_such_realm, <<"wamp.error.no_such_realm">>},
           {no_such_registration, <<"wamp.error.no_such_registration">>},
           {no_such_role, <<"wamp.error.no_such_role">>},
           {no_such_subscription, <<"wamp.error.no_such_subscription">>},
           {not_authorized, <<"wamp.error.not_authorized">>},
+          {option_disclose_me, <<"wamp.error.option_disallowed.disclose_me">>},
+          {option_not_allowed, <<"wamp.error.option_not_allowed">>},
           {procedure_already_exists, <<"wamp.error.procedure_already_exists">>},
           {system_shutdown, <<"wamp.error.system_shutdown">>}
         ]).
