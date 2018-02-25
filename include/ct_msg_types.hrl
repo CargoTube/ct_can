359bdea35411b36c0c38d5b8f94475963d906c16
bwegh <baswegh@gmx.de>
2018-02-25 14:02:04 +0100

    change id from pos_integer to non_neg_integer

--- include/ct_msg_types.hrl
+++ include/ct_msg_types.hrl
@@ -18,46 +18,51 @@
 -type ct_msg_welcome() :: {welcome, non_neg_integer(), map()}.
 -type ct_msg_abort() :: {abort, map(), ct_msg_error_uri()}.
 -type ct_msg_goodbye() :: {goodbye, map(), ct_msg_error_uri()}.
--type ct_msg_error() :: {error, ct_msg_type(), pos_integer(), map(),
+-type ct_msg_error() :: {error, ct_msg_type(), non_neg_integer(), map(),
                           ct_msg_error_uri()} |
-                      {error, ct_msg_type(), pos_integer(), map(),
+                      {error, ct_msg_type(), non_neg_integer(), map(),
                         ct_msg_error_uri(), list()} |
-                      {error, ct_msg_type(), pos_integer(), map(),
+                      {error, ct_msg_type(), non_neg_integer(), map(),
                         ct_msg_error_uri(), list(), map()} .
--type ct_msg_publish() :: {publish, pos_integer(), map(), ct_msg_uri()}|
-                          {publish, pos_integer(), map(), ct_msg_uri(), list()}|
-                          {publish, pos_integer(), map(), ct_msg_uri(), list(),
-                           map()}.
--type ct_msg_published() :: {published, pos_integer(), pos_integer()}.
--type ct_msg_subscribe() :: {subscribe, pos_integer(), map(), ct_msg_uri()}.
--type ct_msg_subscribed() :: {subscribed, pos_integer(), pos_integer()}.
--type ct_msg_unsubscribe() :: {unsubscribe, pos_integer(), pos_integer()}.
--type ct_msg_unsubscribed() :: {unsubscribed, pos_integer()}.
--type ct_msg_event() :: {event, pos_integer(), pos_integer(), map()} |
-                        {event, pos_integer(), pos_integer(), map(), list()} |
-                        {event, pos_integer(), pos_integer(), map(), list(),
-                         map()}.
--type ct_msg_call() :: {call, pos_integer(), map(), ct_msg_uri()} |
-                       {call, pos_integer(), map(), ct_msg_uri(), list()} |
-                       {call, pos_integer(), map(), ct_msg_uri(), list(), map()}
+-type ct_msg_publish() :: {publish, non_neg_integer(), map(), ct_msg_uri()}|
+                          {publish, non_neg_integer(), map(), ct_msg_uri(),
+                           list()}|
+                          {publish, non_neg_integer(), map(), ct_msg_uri(),
+                           list(), map()}.
+-type ct_msg_published() :: {published, non_neg_integer(), non_neg_integer()}.
+-type ct_msg_subscribe() :: {subscribe, non_neg_integer(), map(), ct_msg_uri()}.
+-type ct_msg_subscribed() :: {subscribed, non_neg_integer(), non_neg_integer()}.
+-type ct_msg_unsubscribe() :: {unsubscribe, non_neg_integer(),
+                               non_neg_integer()}.
+-type ct_msg_unsubscribed() :: {unsubscribed, non_neg_integer()}.
+-type ct_msg_event() :: {event, non_neg_integer(), non_neg_integer(), map()} |
+                        {event, non_neg_integer(), non_neg_integer(), map(),
+                         list()} |
+                        {event, non_neg_integer(), non_neg_integer(), map(),
+                         list(), map()}.
+-type ct_msg_call() :: {call, non_neg_integer(), map(), ct_msg_uri()} |
+                       {call, non_neg_integer(), map(), ct_msg_uri(), list()} |
+                       {call, non_neg_integer(), map(), ct_msg_uri(), list(),
+                        map()}
                        .
--type ct_msg_cancel() :: {cancel, pos_integer(), map()}.
--type ct_msg_result() :: {result, pos_integer(), map()} |
-                         {result, pos_integer(), map(), list()} |
-                         {result, pos_integer(), map(), list(), map()}.
--type ct_msg_register() :: {register, pos_integer(), map(), ct_msg_uri()}.
--type ct_msg_registered() :: {registered, pos_integer(), pos_integer()}.
--type ct_msg_unregister() :: {unregister, pos_integer(), pos_integer()}.
--type ct_msg_unregistered() :: {unregistered, pos_integer()}.
--type ct_msg_invocation() :: {invocation, pos_integer(), pos_integer(), map()} |
-                             {invocation, pos_integer(), pos_integer(), map(),
-                              list()} |
-                             {invocation, pos_integer(), pos_integer(), map(),
-                              list(), map()}.
--type ct_msg_interrupt() :: {interrupt, pos_integer(), map()}.
--type ct_msg_yield() :: {yield, pos_integer(), map()} |
-                        {yield, pos_integer(), map(), list()} |
-                        {yield, pos_integer(), map(), list(), map()} .
+-type ct_msg_cancel() :: {cancel, non_neg_integer(), map()}.
+-type ct_msg_result() :: {result, non_neg_integer(), map()} |
+                         {result, non_neg_integer(), map(), list()} |
+                         {result, non_neg_integer(), map(), list(), map()}.
+-type ct_msg_register() :: {register, non_neg_integer(), map(), ct_msg_uri()}.
+-type ct_msg_registered() :: {registered, non_neg_integer(), non_neg_integer()}.
+-type ct_msg_unregister() :: {unregister, non_neg_integer(), non_neg_integer()}.
+-type ct_msg_unregistered() :: {unregistered, non_neg_integer()}.
+-type ct_msg_invocation() :: {invocation, non_neg_integer(), non_neg_integer(),
+                              map()} |
+                             {invocation, non_neg_integer(), non_neg_integer(),
+                              map(), list()} |
+                             {invocation, non_neg_integer(), non_neg_integer(),
+                              map(), list(), map()}.
+-type ct_msg_interrupt() :: {interrupt, non_neg_integer(), map()}.
+-type ct_msg_yield() :: {yield, non_neg_integer(), map()} |
+                        {yield, non_neg_integer(), map(), list()} |
+                        {yield, non_neg_integer(), map(), list(), map()} .
 
 
 -type ct_msg() :: ct_msg_hello() |
